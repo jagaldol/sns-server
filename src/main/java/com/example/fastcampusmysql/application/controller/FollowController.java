885e4e0716fa036cccbeefcfb5bbb35c26f1cab1
jagaldol <yoy06056@pusan.ac.kr>
2023-06-02 02:42:09 +0900

    feat: Implement get /follow/members/{fromId}
    팔로잉한 목록을 리턴한다.

--- src/main/java/com/example/fastcampusmysql/application/controller/FollowController.java
+++ src/main/java/com/example/fastcampusmysql/application/controller/FollowController.java
@@ -1,11 +1,12 @@
 package com.example.fastcampusmysql.application.controller;
 
 import com.example.fastcampusmysql.application.usecase.CreateFollowMemberUsecase;
+import com.example.fastcampusmysql.application.usecase.GetFollowingMembersUsecase;
+import com.example.fastcampusmysql.domain.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,8 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class FollowController {
     final private CreateFollowMemberUsecase createFollowMemberUsecase;
 
+    final private GetFollowingMembersUsecase getFollowingMembersUsecase;
+
     @PostMapping("/{fromId}/{toId}")
     public void create(@PathVariable Long fromId, @PathVariable Long toId) {
         createFollowMemberUsecase.execute(fromId, toId);
+    }
+
+    @GetMapping("/members/{fromId}")
+    public List<MemberDto> getFollowing(@PathVariable Long fromId) {
+        return getFollowingMembersUsecase.execute(fromId);
     }
 }
