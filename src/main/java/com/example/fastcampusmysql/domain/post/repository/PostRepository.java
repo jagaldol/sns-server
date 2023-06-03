26dd77e5e2e76fb1abd6fc0a6138b26d9285a301
jagaldol <yoy06056@pusan.ac.kr>
2023-06-03 21:52:36 +0900

    feat: Create Post entity and repository
    post doesn't support update

---
+++ src/main/java/com/example/fastcampusmysql/domain/post/repository/PostRepository.java
@@ -0,0 +1,60 @@
+package com.example.fastcampusmysql.domain.post.repository;
+
+import com.example.fastcampusmysql.domain.post.entity.Post;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Repository
+public class PostRepository {
+    final private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    final static private String TABLE = "Post";
+
+    final static RowMapper<Post> rowMapper = (ResultSet resultSet, int rowNum) -> Post
+            .builder()
+            .id(resultSet.getLong("id"))
+            .memberId(resultSet.getLong("memberId"))
+            .contents(resultSet.getString("contents"))
+            .createdDate(resultSet.getObject("createdDate", LocalDate.class))
+            .createdAt(resultSet.getObject("createdAt", LocalDateTime.class))
+            .build();
+
+    public Post save(Post post) {
+        /*
+            post id를 보고 갱신 또는 삽입을 정함
+            반환값은 id를 담아서 반환한다.
+         */
+        if (post.getId() == null) {
+            return insert(post);
+        }
+        throw new UnsupportedOperationException("Post는 갱신을 지원하지 않습니다.");
+    }
+
+    private Post insert(Post post) {
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(namedParameterJdbcTemplate.getJdbcTemplate())
+                .withTableName(TABLE)
+                .usingGeneratedKeyColumns("id");
+
+        SqlParameterSource params = new BeanPropertySqlParameterSource(post);
+        var id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
+        return Post
+                .builder()
+                .id(id)
+                .memberId(post.getMemberId())
+                .contents(post.getContents())
+                .createdDate(post.getCreatedDate())
+                .createdAt(post.getCreatedAt())
+                .build();
+    }
+
+}
