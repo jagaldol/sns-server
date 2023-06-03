0157e437dcc05797d3fde70074242e60fcd3e2ef
jagaldol <yoy06056@pusan.ac.kr>
2023-06-04 04:20:35 +0900

    test: Large Input Processing Test

---
+++ src/test/java/com/example/fastcampusmysql/domain/post/PostBulkInsertTest.java
@@ -0,0 +1,50 @@
+package com.example.fastcampusmysql.domain.post;
+
+import com.example.fastcampusmysql.domain.post.entity.Post;
+import com.example.fastcampusmysql.domain.post.repository.PostRepository;
+import com.example.fastcampusmysql.util.PostFixtureFactory;
+import org.jeasy.random.EasyRandomParameters;
+import org.jeasy.random.randomizers.range.LocalDateRangeRandomizer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StopWatch;
+
+import java.time.LocalDate;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class PostBulkInsertTest {
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    public void bulkInsert() {
+        var easyRandom = PostFixtureFactory.get(
+                3L,
+                LocalDate.of(2023, 5, 1),
+                LocalDate.of(2023, 6, 1)
+        );
+
+        var stopWatch = new StopWatch();
+        stopWatch.start();
+
+        int _1만 = 10000;
+        var posts = IntStream.range(0, _1만)
+                .parallel()
+                .mapToObj(i -> easyRandom.nextObject(Post.class))
+                .toList();
+
+        stopWatch.stop();
+        System.out.println("객체 생성 시간: " + stopWatch.getTotalTimeSeconds());
+
+        var queryStopWatch = new StopWatch();
+        queryStopWatch.start();
+
+        postRepository.bulkInsert(posts);
+
+        queryStopWatch.stop();
+
+        System.out.println("db insert 시간: " + queryStopWatch.getTotalTimeSeconds());
+    }
+}
