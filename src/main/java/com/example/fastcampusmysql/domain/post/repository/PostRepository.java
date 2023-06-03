7e10eadba725dee3e65259b46d81ff501f788897
jagaldol <yoy06056@pusan.ac.kr>
2023-06-03 22:54:18 +0900

    feat: Implment POST Count by Calendar

--- src/main/java/com/example/fastcampusmysql/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/fastcampusmysql/domain/post/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.example.fastcampusmysql.domain.post.repository;
 
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
 import com.example.fastcampusmysql.domain.post.entity.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Repository;
 import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Repository
@@ -28,6 +31,24 @@ public class PostRepository {
             .createdDate(resultSet.getObject("createdDate", LocalDate.class))
             .createdAt(resultSet.getObject("createdAt", LocalDateTime.class))
             .build();
+
+    final static RowMapper<DailyPostCount> DAILY_POST_COUNT_ROW_MAPPER = (ResultSet resultSet, int rowNum) -> new DailyPostCount(
+            resultSet.getLong("memberId"),
+            resultSet.getObject("createdDate", LocalDate.class),
+            resultSet.getLong("count")
+    );
+
+    public List<DailyPostCount> groupByCreatedDate(DailyPostCountRequest request) {
+        var sql = String.format("""
+                SELECT createdDate, memberId, count(id) as count
+                FROM %s
+                WHERE memberId = :memberId and createdDate between :firstDate and :lastDate
+                GROUP BY createdDate, memberId
+                """, TABLE);
+
+        var params = new BeanPropertySqlParameterSource(request);
+        return namedParameterJdbcTemplate.query(sql, params, DAILY_POST_COUNT_ROW_MAPPER);
+    }
 
     public Post save(Post post) {
         /*
