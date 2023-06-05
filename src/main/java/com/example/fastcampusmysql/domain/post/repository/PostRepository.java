5d00227ce3a9cb4dc57f133edfad24eb23aaa0b2
jagaldol <yoy06056@pusan.ac.kr>
2023-06-05 21:50:13 +0900

    refactor: Move PageHelper to util package

--- src/main/java/com/example/fastcampusmysql/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/fastcampusmysql/domain/post/repository/PostRepository.java
@@ -1,13 +1,12 @@
 package com.example.fastcampusmysql.domain.post.repository;
 
-import com.example.fastcampusmysql.domain.PageHelper;
+import com.example.fastcampusmysql.util.PageHelper;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
 import com.example.fastcampusmysql.domain.post.entity.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
